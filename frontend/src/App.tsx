b0bead4f5bc035a3eb2852144ea7e1f67ff2f2bf
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-10-09 01:00:00 +0300

    added header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import Header from "./components/Header";
 
 const App = () => {
     return (
-        <>
+        <div className="container mx-auto px-8">
             <Header />
-        </>
+        </div>
     );
 };
 
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
-        <>
-            <p className="text-center text-green-500">fab.two</p>
-        </>
+        <header className="flex justify-between py-6">
+            <Link
+                className="font-bold font-lora text-4xl text-light tracking-wider"
+                to="/"
+            >
+                fab.two
+            </Link>
+            <div className="flex font-montserrat items-center space-x-6 text-sm tracking-widest">
+                <Link to="/blogs">BLOG</Link>
+                <span className="select-none text-gray-400">|</span>
+                <Link to="/ratings">RATINGS</Link>
+            </div>
+        </header>
     );
 };
 
