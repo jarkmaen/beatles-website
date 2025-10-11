fc664e1e380e0649cdc16a1ab58948238bd99aed
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-10-11 14:25:31 +0300

    basic songs router

---
+++ backend/controllers/songs.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { Song } from "../models/index.js";
+
+const router = Router();
+
+router.get("/", async (_req, res) => {
+    try {
+        const songs = await Song.findAll();
+        res.json(songs);
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+router.get("/random", async (_req, res) => {
+    try {
+        const count = await Song.count();
+
+        if (count === 0) {
+            res.status(404).json({ error: "No songs found" });
+            return;
+        }
+
+        const randomID = Math.floor(Math.random() * count) + 1;
+        const song = await Song.findByPk(randomID);
+
+        res.json(song);
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
