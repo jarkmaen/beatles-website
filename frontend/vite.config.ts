3cd4c8bd73414ae1b969ad35e045dac62fc28e9d
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-10-12 22:36:12 +0300

    vite proxy for backend API requests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            "/api": { changeOrigin: true, target: "http://localhost:3000" }
+        }
+    }
 });
