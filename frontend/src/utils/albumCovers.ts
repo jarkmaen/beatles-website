5935a2d70b8794e8a8b66630d34ca2612fc6ad12
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-10-13 23:17:39 +0300

    album cover mapping util

---
+++ frontend/src/utils/albumCovers.ts
@@ -0,0 +1,33 @@
+import abbeyRoadCover from "../assets/images/abbey_road.jpg";
+import beatlesForSaleCover from "../assets/images/beatles_for_sale.jpg";
+import hardDaysNightCover from "../assets/images/a_hard_days_night.jpg";
+import helpCover from "../assets/images/help.png";
+import letItBeCover from "../assets/images/let_it_be.png";
+import letItBeNakedCover from "../assets/images/let_it_be_naked.jpg";
+import magicalMysteryTourCover from "../assets/images/magical_mystery_tour.jpg";
+import pastMastersCover from "../assets/images/past_masters.png";
+import pleasePleaseMeCover from "../assets/images/please_please_me.png";
+import revolverCover from "../assets/images/revolver.jpg";
+import rubberSoulCover from "../assets/images/rubber_soul.png";
+import sgtPepperCover from "../assets/images/sgt_pepper.jpg";
+import whiteAlbumCover from "../assets/images/the_beatles.jpg";
+import withTheBeatlesCover from "../assets/images/with_the_beatles.png";
+import yellowSubmarineCover from "../assets/images/yellow_submarine.jpg";
+
+export const albumCoverMap: Record<string, string> = {
+    "A Hard Day's Night": hardDaysNightCover,
+    "Abbey Road": abbeyRoadCover,
+    "Beatles for Sale": beatlesForSaleCover,
+    "Help!": helpCover,
+    "Let It Be": letItBeCover,
+    "Let It Be... Naked": letItBeNakedCover,
+    "Magical Mystery Tour": magicalMysteryTourCover,
+    "Please Please Me": pleasePleaseMeCover,
+    Revolver: revolverCover,
+    "Rubber Soul": rubberSoulCover,
+    "Sgt. Pepper's Lonely Hearts Club Band": sgtPepperCover,
+    Single: pastMastersCover,
+    "The Beatles (White Album)": whiteAlbumCover,
+    "With the Beatles": withTheBeatlesCover,
+    "Yellow Submarine": yellowSubmarineCover
+};
