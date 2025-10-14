64e459aad3403962c2c58e4532ba66cff6707251
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-10-14 23:33:51 +0300

    added ratingCategory const

---
+++ frontend/src/constants/ratingCategories.ts
@@ -0,0 +1,11 @@
+export const ratingCategory = {
+    bassline: { label: "Bassline", max: 5 },
+    chord_progression: { label: "Chord Progression", max: 4 },
+    cultural_significance: { label: "Cultural Significance", max: 1 },
+    full_instrumentation: { label: "Full Instrumentation", max: 10 },
+    lyrics: { label: "Lyrics", max: 8 },
+    originality_innovation: { label: "Originality/Innovation", max: 8 },
+    percussion: { label: "Percussion", max: 5 },
+    solo: { label: "Solo", max: 4 },
+    vocals: { label: "Vocals", max: 10 }
+};
