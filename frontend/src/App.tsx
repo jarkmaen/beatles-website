68a936d86ca80785976c63b7b08b0fc061306c62
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-10-24 18:25:23 +0300

    made py-12 CSS ruling conditional as well

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,21 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Ratings from "./components/Ratings";
 import useInitialization from "./hooks/useInitialization";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
 const App = () => {
     const stateInitializer = useInitialization();
+    const location = useLocation();
 
     useEffect(() => {
         stateInitializer();
     }, []);
 
-    const containerClasses = "container mx-auto px-8";
     const isRatingsPage = location.pathname === "/ratings";
+
+    const containerClasses = "container mx-auto px-8";
+    const mainClasses = isRatingsPage ? "" : "py-12";
 
     return (
         <>
@@ -22,7 +25,7 @@ const App = () => {
                 <Header />
             </div>
             <div className={isRatingsPage ? "" : containerClasses}>
-                <main className="py-12">
+                <main className={mainClasses}>
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/blogs" element={<BlogPost />} />
