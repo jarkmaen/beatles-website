f0dbab59f75d3d7bd0fb170d5abfd0f63144ccfa
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-11-04 20:22:51 +0200

    updated albums constant to have label and value

--- frontend/src/constants/albums.ts
+++ frontend/src/constants/albums.ts
@@ -1,19 +1,22 @@
 export const albums = [
-    "A Hard Day's Night",
-    "Abbey Road",
-    "Beatles for Sale",
-    "Help!",
-    "Let It Be",
-    "Let It Be... Naked",
-    "Magical Mystery Tour",
-    "Please Please Me",
-    "Revolver",
-    "Rubber Soul",
-    "Sgt. Pepper's Lonely Hearts Club Band",
-    "Singles",
-    "The Beatles (White Album)",
-    "With the Beatles",
-    "Yellow Submarine"
+    { label: "A Hard Day's Night", value: "A Hard Day's Night" },
+    { label: "Abbey Road", value: "Abbey Road" },
+    { label: "Beatles for Sale", value: "Beatles for Sale" },
+    { label: "Help!", value: "Help!" },
+    { label: "Let It Be", value: "Let It Be" },
+    { label: "Let It Be... Naked", value: "Let It Be... Naked" },
+    { label: "Magical Mystery Tour", value: "Magical Mystery Tour" },
+    { label: "Please Please Me", value: "Please Please Me" },
+    { label: "Revolver", value: "Revolver" },
+    { label: "Rubber Soul", value: "Rubber Soul" },
+    {
+        label: "Sgt. Pepper's Lonely Hearts Club Band",
+        value: "Sgt. Pepper's Lonely Hearts Club Band"
+    },
+    { label: "Singles", value: "Single" },
+    { label: "The Beatles (White Album)", value: "The Beatles (White Album)" },
+    { label: "With the Beatles", value: "With the Beatles" },
+    { label: "Yellow Submarine", value: "Yellow Submarine" }
 ] as const;
 
 export type Album = (typeof albums)[number];
