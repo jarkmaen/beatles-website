0a936b381f294528445fb6b835f7a31f8053a1fc
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-12-21 16:53:13 +0200

    close hamburger menu on outside click

--- frontend/src/components/HamburgerMenu.tsx
+++ frontend/src/components/HamburgerMenu.tsx
@@ -1,12 +1,33 @@
 import Divider from "./Divider";
 import HeaderNavLink from "./HeaderNavLink";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const HamburgerMenu = () => {
     const [isOpen, setOpen] = useState(false);
 
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+            if (
+                menuRef.current &&
+                !menuRef.current.contains(event.target as Node)
+            ) {
+                setOpen(false);
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isOpen]);
+
     return (
-        <>
+        <div className="flex items-center justify-center" ref={menuRef}>
             <button
                 className="dark:hover:text-primary-dark dark:text-secondary-dark flex hover:text-primary-light items-center justify-center sm:hidden text-secondary-light"
                 onClick={() => setOpen((prev) => !prev)}
@@ -14,7 +35,7 @@ const HamburgerMenu = () => {
                 <span className="material-symbols-outlined">dehaze</span>
             </button>
             {isOpen && (
-                <div className="absolute bg-surface-light border border-border-light dark:bg-surface-dark dark:border-border-dark mr-0 p-4 right-4 rounded-lg shadow-xl top-14 w-48 z-10">
+                <div className="absolute bg-surface-light border border-border-light dark:bg-surface-dark dark:border-border-dark mr-0 p-4 right-4 rounded-lg shadow-xl top-[52px] w-48 z-10">
                     <div className="flex-col font-montserrat text-sm tracking-widest">
                         <HeaderNavLink label="BLOG" to="/blogs" />
                         <Divider marginSize={4} />
@@ -24,7 +45,7 @@ const HamburgerMenu = () => {
                     </div>
                 </div>
             )}
-        </>
+        </div>
     );
 };
 
