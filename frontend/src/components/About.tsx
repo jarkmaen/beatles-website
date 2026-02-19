fbacd19f973e8868428c24dbc633ecde3f16960a
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-02-19 18:19:52 +0200

    updated About page to have actual text

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -47,29 +47,20 @@ const About = () => {
             </h1>
             <div className="dark:prose-invert hyphens-auto max-w-none prose sm:prose-lg">
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    Fab Two is a collaboration between two Beatles enthusiasts,
+                    Jarkko and Jonas. Out of love for the music of The Beatles,
+                    and with a zest for spreadsheets and talking endlessly about
+                    music, we undertook the project of rating and ranking all
+                    their original songs. This site is our platform to share the
+                    results and any other future endeavors linked to The
+                    Beatles.
                 </p>
                 <p>
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptatem accusantium doloremque laudantium, totam rem
-                    aperiam, eaque ipsa quae ab illo inventore veritatis et
-                    quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                    enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-                    aut fugit, sed quia consequuntur magni dolores eos qui
-                    ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-                    qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                    velit, sed quia non numquam eius modi tempora incidunt ut
-                    labore et dolore magnam aliquam quaerat voluptatem. Ut enim
-                    ad minima veniam, quis nostrum exercitationem ullam corporis
-                    suscipit laboriosam, nisi ut aliquid ex ea commodi
-                    consequatur?
+                    Besides the main attraction (the rating and ranking of all
+                    original Beatles songs), you might find future blog posts on
+                    the covers they did, on albums or songs they recorded in
+                    their solo careers... Anything related to The Beatles,
+                    except the gossip!
                 </p>
             </div>
             <hr className="border-divider-light dark:border-divider-dark my-12" />
