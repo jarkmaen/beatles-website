24ed7a9ccfddfc48ee26e429ee2cfac8a42ac35d
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-03-09 19:28:58 +0200

    fixed scrollbar gutter issues

--- frontend/src/components/Layouts/MainLayout.tsx
+++ frontend/src/components/Layouts/MainLayout.tsx
@@ -5,13 +5,17 @@ const MainLayout = () => {
     const location = useLocation();
 
     return (
-        <div className="container lg:px-8 mx-auto px-4">
-            <Header key={location.pathname} />
-            <div className="block border-divider-light border-t dark:border-divider-dark ml-[calc(50%_-_50vw)] mr-[calc(50%_-_50vw)] transition w-screen" />
-            <main className="lg:py-12 py-8">
-                <Outlet />
-            </main>
-        </div>
+        <>
+            <div className="container lg:px-8 mx-auto px-4">
+                <Header key={location.pathname} />
+            </div>
+            <div className="border-divider-light border-t dark:border-divider-dark transition" />
+            <div className="container lg:px-8 mx-auto px-4">
+                <main className="lg:py-12 py-8">
+                    <Outlet />
+                </main>
+            </div>
+        </>
     );
 };
 
--- frontend/src/components/Layouts/RatingsLayout.tsx
+++ frontend/src/components/Layouts/RatingsLayout.tsx
@@ -6,8 +6,8 @@ const RatingsLayout = () => {
         <div className="flex flex-col h-screen">
             <div className="container lg:px-8 mx-auto px-4">
                 <Header />
-                <div className="block border-divider-light border-t dark:border-divider-dark ml-[calc(50%_-_50vw)] mr-[calc(50%_-_50vw)] transition w-screen" />
             </div>
+            <div className="border-divider-light border-t dark:border-divider-dark transition" />
             <main className="flex flex-col min-h-0">
                 <Outlet />
             </main>
