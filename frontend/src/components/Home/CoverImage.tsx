1606b4c63c852cae2dfa7eae68daef723ccc54df
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-03-13 14:50:03 +0200

    added animate-pulse back to CoverImage

--- frontend/src/components/Home/CoverImage.tsx
+++ frontend/src/components/Home/CoverImage.tsx
@@ -9,11 +9,13 @@ const CoverImage = ({ alt, src }: Props) => {
     const [isLoaded, setIsLoaded] = useState(false);
 
     return (
-        <div className="bg-gray-200 dark:bg-gray-700 h-48 lg:h-56 lg:w-56 md:h-52 md:w-52 overflow-hidden rounded-lg shadow-2xl shrink-0 w-48">
+        <div
+            className={`${!isLoaded ? "animate-pulse" : ""} bg-gray-200 dark:bg-gray-700 h-48 lg:h-56 lg:w-56 md:h-52 md:w-52 overflow-hidden rounded-lg shadow-2xl shrink-0 w-48`}
+        >
             <img
                 alt={alt}
                 className={`
-                    h-full object-cover ${isLoaded ? "opacity-100" : "opacity-0"} transition w-full
+                    h-full object-cover ${isLoaded ? "opacity-100" : "opacity-0"} w-full
                 `}
                 fetchPriority="high"
                 onLoad={() => setIsLoaded(true)}
