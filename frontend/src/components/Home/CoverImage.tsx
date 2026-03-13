74ab227d6a52444623be7767dcd2d27bf60806c3
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-03-13 12:36:27 +0200

    improved LCP discovery for CoverImage

--- frontend/src/components/Home/CoverImage.tsx
+++ frontend/src/components/Home/CoverImage.tsx
@@ -9,22 +9,16 @@ const CoverImage = ({ alt, src }: Props) => {
     const [isLoaded, setIsLoaded] = useState(false);
 
     return (
-        <div className="h-48 lg:h-56 lg:w-56 md:h-52 md:w-52 relative shrink-0 w-48">
+        <div className="bg-gray-200 dark:bg-gray-700 h-48 lg:h-56 lg:w-56 md:h-52 md:w-52 overflow-hidden rounded-lg shadow-2xl shrink-0 w-48">
             <img
                 alt={alt}
+                className={`
+                    h-full object-cover ${isLoaded ? "opacity-100" : "opacity-0"} transition w-full
+                `}
+                fetchPriority="high"
                 onLoad={() => setIsLoaded(true)}
                 src={src}
-                style={{ display: "none" }}
             />
-            {isLoaded ? (
-                <img
-                    alt={alt}
-                    className="h-48 lg:h-56 lg:w-56 md:h-52 md:w-52 object-cover rounded-lg shadow-2xl w-48"
-                    src={src}
-                />
-            ) : (
-                <div className="absolute animate-pulse bg-gray-200 dark:bg-gray-700 h-48 inset-0 lg:h-56 lg:w-56 md:h-52 md:w-52 object-cover rounded-lg shadow-2xl w-48" />
-            )}
         </div>
     );
 };
